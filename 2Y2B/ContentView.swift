fdad8741db3311cf7fb6e6d903e754bf0caf7bea
mycoalchen <chen.michael.1116@gmail.com>
2024-02-02 17:39:19 -0500

    add gitignore

--- 2Y2B/ContentView.swift
+++ 2Y2B/ContentView.swift
@@ -7,15 +7,48 @@
 
 import SwiftUI
 
+let backgroundColor : Color = Color(red:30/255, green:11/255, blue:4/255)
+let offWhite1 : Color = Color(red:246/255, green:241/255, blue:237/255)
+let offWhite2 : Color = Color(red: 196/255, green: 186/255, blue: 182/255)
+
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var nameWidth: CGFloat = 0
+    @FocusState private var nameFocused: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0.0) {
+            Text("2Y2B")
+                .foregroundStyle(offWhite1)
+                .font(.custom("EBGaramond-Regular", size: 46))
+            HStack(spacing: 0) {
+                Text("[")
+                    .foregroundColor(offWhite2)
+                    .font(.custom("EBGaramond-Regular", size: 18))
+                TextField("", text: $name, prompt: Text("Your name").foregroundColor(offWhite2), axis: .horizontal)
+                    .autocorrectionDisabled()
+                    .accentColor(offWhite2)
+                    .foregroundColor(offWhite2)
+                    .font(.custom("EBGaramond-Regular", size: 18))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 0)
+                    .fixedSize()
+                    .frame(idealWidth: 0)
+                    .focused($nameFocused)
+                    .onChange(of: nameFocused) {
+                        print(nameFocused)
+                        print(name)
+                    }
+                Text("]")
+                    .foregroundColor(offWhite2)
+                    .font(.custom("EBGaramond-Regular", size: 18))
+            }
+            .frame(minWidth: 20)
+            
         }
-        .padding()
+        .padding(0.0)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(backgroundColor)
     }
 }
 
