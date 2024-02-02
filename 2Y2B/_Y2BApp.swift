bbd1f0371304917cc17f891fee901017187ce41b
mycoalchen <chen.michael.1116@gmail.com>
2024-02-02 17:09:16 -0500

    Initial Commit

---
+++ 2Y2B/_Y2BApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Y2BApp.swift
+//  2Y2B
+//
+//  Created by Michael Chen on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct _Y2BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
